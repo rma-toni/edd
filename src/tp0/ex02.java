f9109a43c07030c85c5657d106aac29ed713e23a
rma-toni <rmaflores.sw@gmail.com>
2025-08-18 12:31:40 -0300

    update (ex02) - rma-toni - 18/08/2025

--- src/tp0/ex02.java
+++ src/tp0/ex02.java
@@ -2,17 +2,14 @@ package tp0;
 
 import java.util.Scanner;
 
+//NOTA: funciona, completar la parte en cual se calcula cuantas veces la suma fue par o impar
+
 public class ex02 {
 
     public static void main(String[] args) {
         System.out.println("--LANZAMIENTO DE DADOS--");
         int lanzamientos = ingresoDatos();
-
-        //int randomNumber = ((int) (Math.random() * 6)) + 1;
-
-        while(lanzamientos > 0){
-            lanzamientos = lanzamientos - 1;
-        }
+        lanzarDados(lanzamientos);
     }
 
     public static int ingresoDatos(){
@@ -30,5 +27,27 @@ public class ex02 {
             }
         }
         return lanzamientos;
+    }
+
+    public static void lanzarDados(int lanzamientos){
+        int suma = 0;
+        float promedios = 0;
+        float aux = lanzamientos;
+        while(lanzamientos > 0){
+            int lanzamiento = 1;
+            int dado1 = ((int) (Math.random() * 6)) + 1;
+            suma += dado1;
+            int dado2 = ((int) (Math.random() * 6)) + 1;
+            suma += dado2;
+            System.out.print("Lanzamiento numero " + lanzamiento+": ");
+            System.out.print("Dado 1: "+dado1+" y Dado 2: "+dado2+"\n");
+
+            //PROMEDIOS
+            promedios = suma / aux;
+
+            lanzamientos = lanzamientos - 1;
+        }
+        System.out.println("La suma de los lanzamientos es igual a : "+suma);
+        System.out.println("La suma promedio de cada lanzamiento es igual a : "+promedios);
     }
 }
