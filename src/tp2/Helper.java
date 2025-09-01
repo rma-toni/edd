3fb7df2cff83d2d3917ff20a7f136c25829310ce
rma-toni <rmaflores.sw@gmail.com>
2025-08-31 21:41:46 -0300

    ejercicio 1 tp 2 terminado - rma-toni - 31/08/2025 21:41

---
+++ src/tp2/Helper.java
@@ -0,0 +1,211 @@
+package tp2;
+
+//
+// Created by Julio Tentor <jtentor@fi.unju.edu.ar>
+//
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Helper {
+
+
+    //region Static Objects
+    static Random random = new Random();
+    static Scanner scanner = new Scanner(System.in);
+    //endregion
+
+
+    //region Character Helpers
+    public static Character getCharacter(Scanner scanner, String inputMessage, String errorMessage) {
+        Character characterValue;
+        while (true) {
+            System.out.print(inputMessage);
+            try {
+                characterValue = scanner.nextLine().charAt(0);
+                return characterValue;
+            } catch (Exception exception) {
+                System.out.println(errorMessage);
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static Character getCharacter(Scanner scanner, String inputMessage) {
+        return getCharacter(scanner, inputMessage, "Ingrese un caracter válido");
+    }
+
+    public static Character getCharacter(String inputMessage, String errorMessage) {
+        return getCharacter(Helper.scanner, inputMessage, errorMessage);
+    }
+
+    public static Character getCharacter(String inputMessage) {
+        return getCharacter(Helper.scanner, inputMessage, "Ingrese un caracter válido");
+    }
+    //endregion
+
+
+    //region Integer Helpers
+    private static Integer getInteger(Scanner scanner, String inputMessage, String errorMessage) {
+        Integer integerValue = 0;
+        while (true) {
+            try {
+                System.out.print(inputMessage);
+                integerValue = Integer.parseInt(scanner.nextLine());
+                return integerValue;
+            } catch (Exception exception) {
+                System.out.println(errorMessage);
+            }
+        }
+    }
+
+    private static Integer getInteger(Scanner scanner, String inputMessage) {
+        return getInteger(scanner, inputMessage, "Ingrese un número válido");
+    }
+
+    private static Integer getInteger(String inputMessage, String errorMessage) {
+        return getInteger(Helper.scanner, inputMessage, errorMessage);
+    }
+
+    public static Integer getInteger(String inputMessage) {
+        return getInteger(Helper.scanner, inputMessage, "Ingrese un número válido");
+    }
+    //endregion
+
+
+    //region Double Helpers
+    public static Double getDouble(Scanner scanner, String inputMessage, String errorMessage) {
+        Double doubleValue = 0.0;
+        while (true) {
+            try {
+                System.out.print(inputMessage);
+                doubleValue = Double.parseDouble(scanner.nextLine());
+                return doubleValue;
+            } catch (Exception exception) {
+                System.out.println(errorMessage);
+            }
+        }
+    }
+
+    public static Double getDouble(Scanner scanner, String inputMessage) {
+        return getDouble(scanner, inputMessage, "Ingrese un número válido");
+    }
+
+    public static Double getDouble(String inputMessage, String errorMessage) {
+        return getDouble(Helper.scanner, inputMessage, errorMessage);
+    }
+
+    public static Double getDouble(String inputMessage) {
+        return getDouble(Helper.scanner, inputMessage, "Ingrese un número válido");
+    }
+    //endregion
+
+
+    //region Float Helpers
+    public static Float getFloat(Scanner scanner, String inputMessage, String errorMessage) {
+        Float floatValue = 0f;
+        while (true) {
+            try {
+                System.out.print(inputMessage);
+                floatValue = Float.parseFloat(scanner.nextLine());
+                return floatValue;
+            } catch (Exception exception) {
+                System.out.println(errorMessage);
+            }
+        }
+    }
+
+    public static Float getFloat(Scanner scanner, String inputMessage) {
+        return getFloat(scanner, inputMessage, "Ingrese un número válido");
+    }
+
+    public static Float getFloat(String inputMessage, String errorMessage) {
+        return getFloat(Helper.scanner, inputMessage, errorMessage);
+    }
+
+    public static Float getFloat(String inputMessage) {
+        return getFloat(Helper.scanner, inputMessage, "Ingrese un número válido");
+    }
+    //endregion
+
+
+    //region Array Helpers
+
+    static void printOneDimensionArray(String textBefore, Object[] array, String textAfter) {
+        System.out.print(textBefore);
+        System.out.print("[" + array[0]);
+        for (int i = 1; i < array.length; ++i) {
+            System.out.print("," + array[i]);
+        }
+        System.out.print("]");
+        System.out.print(textAfter);
+    }
+
+
+    static void printTwoDimensionArray(String textBefore, Object[][] array, String textAfter) {
+        System.out.print(textBefore);
+
+        System.out.print("[[" + array[0][0]);
+        for (int j = 1; j < array[0].length; ++j) {
+            System.out.print("," + array[0][j]);
+        }
+        System.out.print("]");
+
+        for (int i = 1; i < array.length; ++i) {
+            System.out.print(",[" + array[i][0]);
+            for (int j = 1; j < array[i].length; ++j) {
+                System.out.print("," + array[i][j]);
+            }
+            System.out.print("]");
+        }
+        System.out.print("]");
+        System.out.print(textAfter);
+    }
+    //endregion
+
+
+    //region Enum Helpers
+
+    // from http://stackoverflow.com/questions/13783295/getting-all-names-in-an-enum-as-a-string
+    public static String[] getEnumNames(Class<? extends Enum<?>> e) {
+        return Arrays.toString(e.getEnumConstants()).replaceAll("^.|.$", "").split(", ");
+    }
+    //endregion
+
+    //region String Helpers - Toni:)
+    public static String getString(String inputMessage){
+        System.out.print(inputMessage);
+        String stringValue = scanner.nextLine();
+        return stringValue;
+    }
+    //endregion
+
+    //region LocalDate Helpers - Toni:)
+    //TODO Error handling
+    public static LocalDate getLocalDate(String inputMessage, String separator){
+        int day;
+        int month;
+        int year;
+
+        while (true){
+
+            System.out.print(inputMessage);
+            String[] splitDate = scanner.nextLine().split(separator);
+            day = Integer.parseInt(splitDate[0]);
+            month = Integer.parseInt(splitDate[1]);
+            year = Integer.parseInt(splitDate[2]);
+            //TODO Si el año es bisiesto
+            //TODO Meses
+            boolean dayC = day <= 31 && day > 0;
+            boolean monthC = month <= 12 && month > 0;
+            boolean yearC = year < 2100 && year > 1950;
+            if(dayC && monthC && yearC){
+                break;
+            }
+        }
+        return LocalDate.of(year,month,day);
+    }
+    //endregion
+}
---
+++ src/tp2/ex01/ex01.java
@@ -0,0 +1,116 @@
+package tp2.ex01;
+
+//region INDICACIONES DEL EJERCICIO
+
+
+//Ejercicios propuestos
+//1) Un negocio desea almacenar los precios de los productos que tiene a la venta. Para lograrlo, primero se
+//debe solicitar la cantidad de precios a ingresar, y luego, realizar la carga. A través de un menú de opciones,
+//el programa debe presentar las siguientes operaciones:
+//a) Calcular y mostrar la suma de todos los precios de los productos existentes. LISTO
+//b) Devolver otro arreglo en el que se aplique un descuento del 15% a los precios de los productos cuyo
+//valor supere un monto ingresado por el usuario. LISTO
+//c) Devolver el promedio de los precios haciendo uso de un método cuyo parámetro es el arreglo original. LISTO
+//d) Obtener la cantidad de productos que tienen un precio inferior a un valor ingresado por el usuario. LISTO
+//Indicaciones:
+//Este ejercicio necesita del objeto scanner para ingresar datos por la consola o teclado, se espera que el
+//código controle los problemas que normalmente ocurren al operar con la consola o teclado.
+//Se espera una correcta modularización entre el código que realiza el ingreso y validación de los datos
+//respecto del código que hace lo que se solicita en el ejercicio.
+
+//endregion
+
+import tp2.Helper;
+
+import java.util.Scanner;
+
+public class ex01 {
+    public static void main(String[] args) {
+
+        //region INGRESO DE DATOS
+        int cantidadPrecios = Helper.getInteger("Ingrese la cantidad de precios que desea ingresar: ");
+        float[] precios = new float[cantidadPrecios];
+        float[] preciosDesc;
+        for(int i = 0; i < precios.length; i++){
+            precios[i] = Helper.getFloat("Ingrese el precio numero "+(i+1)+": ");
+        }
+        //endregion
+
+        //region MENU DE OPCIONES
+        int option;
+        while (true){
+            System.out.println("---------- MENU ----------");
+            System.out.println("1 - Calcular la suma de todos los precios.");
+            System.out.println("2 - Aplicar descuento (%15) a partir de un precio ingresado.");
+            System.out.println("3 - Promedio de los precios.");
+            System.out.println("4 - Obtener la cantidad de productos con un precio inferior al indicado.");
+            System.out.println("0 - Salir del programa");
+            option = Helper.getInteger("Ingrese la opción elegida: ");
+            switch (option){
+                case 1:
+                    System.out.println(sumaPrecios(precios));
+                    break;
+                case 2:
+                    preciosDesc = descuento(precios);
+                    break;
+                case 3:
+                    System.out.println("El promedio de los precios es: "+promedio(precios));
+                    break;
+                case 4:
+                    System.out.println("La cantidad de productos que tienen un precio menor es de: "+precioInferior(precios));
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Opción incorrecta.");
+            }
+        }
+        //endregion
+    }
+
+    public static float sumaPrecios(float[] precios){
+        float suma = 0;
+        for (float precio : precios) {
+            suma += precio;
+        }
+        return suma;
+    }
+
+    //TODO Imprimir el nuevo array
+    public static float[] descuento(float[] precios){
+        float precioMinimo = Helper.getFloat("Ingrese el precio a partir del cual se aplicara el descuento (15%): ");
+        float[] preciosDesc = new float[precios.length];
+        float porcentajeDesc = 0.15f;
+        for(int i = 0; i < precios.length; i++){
+            if(precios[i] > precioMinimo){
+                preciosDesc[i] = precios[i]*(1-porcentajeDesc);
+            }else{
+                preciosDesc[i] = precios[i];
+            }
+        }
+        System.out.println("Descuento aplicado a los precios correspondientes!");
+        return preciosDesc;
+    }
+
+    public static float promedio(float[] precios){
+        float promedio = 0;
+        for(float precio : precios){
+            promedio+=precio;
+        }
+        return  promedio / precios.length;
+    }
+
+    //d) Obtener la cantidad de productos que tienen un precio inferior a un valor ingresado por el usuario.
+    //Indicaciones:
+
+    public static int precioInferior(float[] precios){
+        int contador = 0;
+        float precioComparar = Helper.getFloat("Ingrese el precio de consulta: ");
+        for(float precio : precios){
+            if(precio < precioComparar){
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
